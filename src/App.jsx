a29a0d4e61b5d478034ac09dc28e20a91c938b58
fosa-ij <efosaij@gmail.com>
2024-01-23 11:16:41 +0100

    style changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Container } from "react-bootstrap";
 import Header from "./components/header/Header";
 import BgImg from "./assets/header-svg/bgImage.svg";
 import SocialProof from "./components/social-proof/sp";
-import Info from "./components/info/info";
+import Info from "./components/info/Info";
 import Footer from "./components/footer/Footer";
 import Faq from "./components/FAQ/Faq";
 import Subscription from "./components/subscription/Index";
